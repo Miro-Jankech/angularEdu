e7ff873eff9a4c2fe5da4392827638c3fc5a0b06
Miro-Jankech <jankechmiro@gmail.com>
2025-06-03 16:20:02 +0200

    Two way binding applied on all inputs of Add task pop up

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { TaskComponent } from '../tasks/task/task.component'
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({required:true}) userId! : string;
   @Input({required:true}) name!: string;
+  isAddingTask = false;
   tasks = [
   {
     id: 't1',
@@ -43,5 +45,12 @@ export class TasksComponent {
   onCompleteTask(id:string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
-  
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
 }
