8d13af8f346327f9dda23d43199d0567b4f99a07
Miro-Jankech <jankechmiro@gmail.com>
2025-06-06 21:18:49 +0200

    using inject dependency on complete task

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {type NewTaskData } from '../task/task.model';
+import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,23 +11,28 @@ import {type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Output() close = new EventEmitter<void>();
+  @Input({ required: true }) usedId!: string;
+
+  private tasksService = inject(TasksService)
 
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask(
+    {
       title: this.enteredTitle,
       summary: this.enteredSummary,
-      date: this.enteredDate
-    })
+      date: this.enteredDate,
+    },
+      this.usedId
+    );
+  this.close.emit();
   }
-
 }
